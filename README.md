c998e9e78342a8ee4b7098883a5970070c146d2e
suraid121 <tonoyofficial121@gmail.com>
2025-07-22 22:57:42 +0600

    location-section done

--- README.md
+++ README.md
@@ -1,12 +1,5 @@
 # React + Vite
+## Note on Submission
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Due to severe load-shedding on the final day of the assignment, I was unable to submit the project on time. Despite my best efforts to manage the situation, the prolonged power outage significantly disrupted my ability to finalize and upload the required files. I hope you will kindly consider this unforeseen circumstance with understanding.
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
---
+++ src/Location/location.jsx
@@ -0,0 +1,140 @@
+import Header from '../reusable-components/Header';
+import ActDocCard from '../reusable-components/ActDocCard';
+import NewIncidentData from '../db/NewIncidentData';
+const Location = () => {
+    let Time = new Date();
+    let month = Time.getMonth() + 1;
+    let year = Time.getFullYear();
+    let date = Time.getDate();
+    
+    const LocationData = [
+        {
+            src: '/building-1.svg',
+            title: 'Whitechapel Rd.',
+            place: 'Tulare County, Los Angles, CA 23415',
+            price: '$1,456,654.00'
+        },
+        {
+            src: '/building-2.svg',
+            title: 'Whitechapel Rd.',
+            place: 'Tulare County, Los Angles, CA 23415',
+            price: '$1,456,654.00'
+        },
+        {
+            src: '/building-1.svg',
+            title: 'Whitechapel Rd.',
+            place: 'Tulare County, Los Angles, CA 23415',
+            price: '$1,456,654.00'
+        },
+        {
+            src: '/building-1.svg',
+            title: 'Whitechapel Rd.',
+            place: 'Tulare County, Los Angles, CA 23415',
+            price: '$1,456,654.00'
+        }
+
+    ]
+        
+    let title = <p>{year}-{month}-{date} {NewIncidentData.title} </p>
+    let Heading = <div className='flex items-center gap-2'>
+        <img src={NewIncidentData.src} alt={NewIncidentData.src} />
+        {title}
+    </div>
+    return (
+        <>
+            <Header Title={title } Heading={Heading} ButtonTitle={'+ New Location'} className={'w-40 font-bold'} ButtonFun={ ()=> alert('This is not available at this time')} />
+
+            <section>
+                <div className="flex justify-between flex-col-reverse lg:flex-row">
+                    <div className="flex flex-col gap-6 px-8 lg:px-20 py-10">
+                        <div className='flex flex-col gap-10 border-b-2 pb-10 w-full lg:w-[600px] border-b-light-border'>
+                            <div className="flex gap-4">
+                                <img src="/location-two.svg" alt="location" />
+                                <div>
+                                    <p className="text-mid-gray-text text-sm font-medium">Location</p>
+                                    <p className="text-dark-gray-text text-2xl font-bold">{NewIncidentData.location}</p>
+                                </div>
+                            </div>
+                            <div className="flex gap-4">
+                                <img src="/cost.svg" alt="location" />
+                                <div>
+                                    <p className="text-mid-gray-text text-sm font-medium">Approx. Cost:</p>
+                                    <p className="text-dark-gray-text text-2xl font-bold">$60,607,456.00</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='border-b-2 pb-10 pt-4 w-full lg:w-[600px] border-b-light-border'>
+                            <p className='text-dark-gray-text text-sm font-bold mb-3'>Description</p>
+                            <span className='text-base text-mid-gray-text text-wrap font-[400]'>{NewIncidentData.des}</span>
+                        </div>
+                        <div className='border-b-2 pb-20 pt-4 w-full lg:w-[600px] border-b-light-border'>
+                            <div className='flex items-center justify-between'>
+                                <p className='text-dark-gray-text text-sm font-bold mb-3'>Location</p>
+                                <p className='text-mid-gray-text text-sm font-medium underline underline-offset-2 decoration-mid-gray-text mb-3'>See All</p>
+                            </div>
+                            <div className='flex flex-wrap gap-4'>
+                                {LocationData.map((data, i) => (
+                                    <div
+                                        key={i}
+                                        className="relative w-[180px] max-w-[240px] h-[280px] p-3 grow flex flex-col gap-1 bg-white  "
+                                    >
+                                        <div className="relative">
+                                            {/* Image */}
+                                            <img
+                                                src={data.src}
+                                                alt={data.title}
+                                                className="w-full h-auto rounded-lg object-cover"
+                                            />
+                                            <div className="absolute top-4 right-4 flex items-center gap-2 bg-white text-deep-dark text-sm font-semibold px-3 py-1 rounded-full shadow-sm">
+                                                <img
+                                                    src="/building-logo.svg"
+                                                    alt="icon"
+                                                    className="w-4 h-4 object-cover"
+                                                />
+                                                <span>Blizzard</span>
+                                            </div>
+                                        </div>
+
+                                        {/* Content */}
+                                        <h3 className="text-xl text-dark-gray-text font-bold">{data.title}</h3>
+                                        <p className="text-mid-gray-text text-sm font-medium">{data.place}</p>
+                                        <h3 className="text-md text-dark-gray-text font-medium">{data.price}</h3>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className='flex flex-col gap-2 px-10 mx-auto py-10'>
+                        <p className="text-sm text-mid-gray-text font-medium">Incident Map</p>
+                        <img src="/incident-map.svg" alt="incident-map" className='w-auto h-auto object-cover ' />
+                        <p className="text-sm text-mid-gray-text font-medium">Start 19.1232, -118.233 End 19.3245, -119.2323</p>
+                    </div>
+                </div>
+            </section>
+            <section className=' px-8 lg:px-20 pb-30'>
+                <div className='border-b-2 pb-10 space-y-4 pt-4 w-full lg:w-[600px] border-b-light-border '>
+                    <div className='flex items-center justify-between'>
+                        <p className='text-dark-gray-text text-sm font-bold mb-3'>Activities</p>
+                        <p className='text-dark-gray-text text-sm font-medium underline underline-offset-2 decoration-dark-gray-text mb-3 cursor-pointer ' onClick={()=> alert('This is not available at this time')} >See All</p>
+                    </div>
+                    {Array.from(Array(2).keys()).map((el) => (
+                        <ActDocCard key={el} title={'Activity name'} />
+                    ))}
+                </div>
+                <div className=' pb-10 space-y-4 pt-4 w-full lg:w-[600px] '>
+                    <div className='flex items-center justify-between'>
+                        <p className='text-dark-gray-text text-sm font-bold mb-3'>Documents</p>
+                        <p className='text-dark-gray-text text-sm font-medium underline underline-offset-2 cursor-pointer decoration-dark-gray-text mb-3' onClick={()=> alert('This is not available at this time')} >See All</p>
+                    </div>
+                    {Array.from(Array(2).keys()).map((el) => (
+                        <ActDocCard key={el} title={'Document  name'} />
+                    ))}
+                </div>
+            </section>
+        </>
+    )
+};
+
+export default Location;
